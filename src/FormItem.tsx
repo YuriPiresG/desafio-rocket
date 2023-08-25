44f191c1d58c8d96c11b85b7eb83ab8cc7437f47
Yuri Pires <yuripiresbr@gmail.com>
2023-08-25 18:01:53 -0300

    feat: form is now working

--- src/FormItem.tsx
+++ src/FormItem.tsx
@@ -1,11 +1,30 @@
 import { Plus } from "lucide-react";
 import { useForm } from "react-hook-form";
 
+interface Item {
+  name: string;
+  quantity: number;
+  unit: string;
+  category: string;
+}
+
 export function FormItem() {
   const { register, handleSubmit } = useForm();
+  const itemList: Item[] = [];
   return (
     <div className="absolute top-[8.5rem] left-[1.5rem] right-[1.5rem] grid">
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <form
+        onSubmit={handleSubmit((data) => {
+          const newItem: Item = {
+            name: data.name,
+            quantity: data.quantity,
+            unit: data.unit,
+            category: data.category,
+          };
+          itemList.push(newItem);
+          console.log(itemList);
+        })}
+      >
         <div className="">
           <label className="text-gray200 font-[400] text-[0.75rem] mb-[.5rem]">
             Item
@@ -13,7 +32,7 @@ export function FormItem() {
           <input
             type="text"
             {...register("name")}
-            className="w-[22.5rem] bg-gray500 rounded-[.375rem] border border-gray300 h-[2.5rem]"
+            className="w-[22.5rem] bg-gray500 rounded-[.375rem] border border-gray300 h-[2.5rem] text-gray200 pl-[.75rem]"
           />
         </div>
 
@@ -24,10 +43,9 @@ export function FormItem() {
             </label>
             <div className="flex">
               <input
-                type="text"
-                placeholder="1"
+                type="number"
                 {...register("quantity")}
-                className="w-[4.15rem] bg-gray500 rounded-l-[.375rem] border border-gray300 h-[2.5rem]"
+                className="w-[4.15rem] bg-gray500 rounded-l-[.375rem] border border-gray300 h-[2.5rem] text-gray200 pl-[.75rem] appearance-none"
               />
               <select
                 className="w-[4.15rem] bg-gray400 rounded-r-[.375rem] border border-gray300 h-[2.5rem] text-gray200 text-[.625rem] focus:appearance-none pl-[.75rem]"
