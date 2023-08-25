01b34022ae6a4b7a45f31b89a203bca1e225f787
Yuri Pires <yuripiresbr@gmail.com>
2023-08-25 17:50:05 -0300

    feat: added react hook form

--- src/FormItem.tsx
+++ src/FormItem.tsx
@@ -1,18 +1,19 @@
 import { Plus } from "lucide-react";
+import { useForm } from "react-hook-form";
 
 export function FormItem() {
+  const { register, handleSubmit } = useForm();
   return (
-    <div className="absolute top-[8.5rem] left-[1.5rem] right-[1.5rem]">
-      <form className="grid justify-center">
-        <div className="grid">
+    <div className="absolute top-[8.5rem] left-[1.5rem] right-[1.5rem] grid">
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <div className="">
           <label className="text-gray200 font-[400] text-[0.75rem] mb-[.5rem]">
             Item
           </label>
           <input
             type="text"
-            name=""
-            id=""
-            className="w-[22.5rem] bg-gray500 rounded-[.375rem] border border-gray300 h-[2rem]"
+            {...register("name")}
+            className="w-[22.5rem] bg-gray500 rounded-[.375rem] border border-gray300 h-[2.5rem]"
           />
         </div>
 
@@ -21,26 +22,41 @@ export function FormItem() {
             <label className="text-gray200 font-[400] text-[0.75rem] mb-[.5rem]">
               Quantidade
             </label>
-            <input
-              type="text"
-              name=""
-              id=""
-              className="w-[4.15rem] bg-gray500 rounded-[.375rem] border border-gray300 h-[2rem]"
-            />
+            <div className="flex">
+              <input
+                type="text"
+                placeholder="1"
+                {...register("quantity")}
+                className="w-[4.15rem] bg-gray500 rounded-l-[.375rem] border border-gray300 h-[2.5rem]"
+              />
+              <select
+                className="w-[4.15rem] bg-gray400 rounded-r-[.375rem] border border-gray300 h-[2.5rem] text-gray200 text-[.625rem] focus:appearance-none pl-[.75rem]"
+                defaultValue={"UN."}
+                {...register("unit")}
+              >
+                <option value="unidade" className="">
+                  UN.
+                </option>
+                <option value="litros">L</option>
+              </select>
+            </div>
           </div>
 
-          <div className="grid ">
+          <div className="grid ml-[.75rem]">
             <label className="text-gray200 font-[400] text-[0.75rem] mb-[.5rem]">
               Categoria
             </label>
-            <input
-              type="text"
-              name=""
-              id=""
-              className="w-[4.15rem] bg-gray500 rounded-[.375rem] border border-gray300 h-[2rem]"
-            />
+            <select
+              className="w-[11.15rem] bg-gray400 rounded-[.375rem] border border-gray300 h-[2.5rem] text-gray200 text-[.875rem] focus:appearance-none pl-[.75rem]"
+              {...register("category")}
+            >
+              <option value="padaria">Padaria</option>
+            </select>
           </div>
-          <button className="w-[2.5rem] h-[2.5rem] rounded-[62.4375rem] bg-purpleLight text-white">
+          <button
+            className="w-[2.5rem] h-[2.5rem] rounded-[62.4375rem] bg-purpleLight text-white mt-[2rem] ml-[.75rem]"
+            type="submit"
+          >
             <Plus className="ml-[.5rem]" />
           </button>
         </div>
